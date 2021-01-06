7cf7120747fb0ef8ef384dbe8dbe2e79e86a1e7d
Jesse Parsons <savvyjesse@aol.com>
2021-01-05 19:01:05 -0500

    add solution

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -5,24 +5,25 @@
 // I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new(); // change to mut here, because we will be passing it into fill_vec as a mutable reference to change in place yet retain ownership
 
-    let mut vec1 = fill_vec(vec0);
+    fill_vec(&mut vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
-    vec1.push(88);
+    // vec1.push(88);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    // println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &mut Vec<i32>) {
+    // modifying in place and not returning anything
+    // let vec = vec;
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    // vec.to_vec()
 }
