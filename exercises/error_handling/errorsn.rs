8e9d0a0e700bdf71a70970861678eef2af0fd836
Jesse Parsons <savvyjesse@aol.com>
2021-01-18 12:14:06 -0500

    add solutions

--- exercises/error_handling/errorsn.rs
+++ exercises/error_handling/errorsn.rs
@@ -17,19 +17,19 @@
 //
 // Execute `rustlings hint errorsn` for hints :)
 
-// I AM NOT DONE
-
 use std::error;
+use std::error::Error;
 use std::fmt;
 use std::io;
 
 // PositiveNonzeroInteger is a struct defined below the tests.
-fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, ???> {
+fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, Box<dyn Error>> {
+    // the ? operator after the function calls that return results convert to that value. to make it into a result as required by the read_and_validate() function signature, we must use Ok(answer) as the return value
     let mut line = String::new();
-    b.read_line(&mut line);
-    let num: i64 = line.trim().parse();
-    let answer = PositiveNonzeroInteger::new(num);
-    answer
+    b.read_line(&mut line)?;
+    let num: i64 = line.trim().parse()?;
+    let answer = PositiveNonzeroInteger::new(num)?;
+    Ok(answer)
 }
 
 //
