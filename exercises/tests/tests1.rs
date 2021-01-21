f4aa99fdc3f9dbdac7d9a57e7a81c06f5e07ce37
Jesse Parsons <savvyjesse@aol.com>
2021-01-20 22:27:22 -0500

    add solutions

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -6,12 +6,14 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests1` for hints :)
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        // assert!(true == false);  fails
+        // assert!(true, false);  passes
+        // assert!(true); passes
+        // assert!((1 + 1) == 2); passes
+        // assert!("krust" == "rust");  fails
     }
 }
--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,12 +2,17 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
-
+fn calculate_num() -> u32 {
+    let num = 1 * 2 * 3 * 4 * 5;
+    num
+}
 #[cfg(test)]
 mod tests {
+    use calculate_num;
     #[test]
+
     fn you_can_assert_eq() {
-        assert_eq!();
+        // assert_eq!(calculate_num(), 120); passes
+        // assert_eq!("Ed", "ed"); fails
     }
 }
